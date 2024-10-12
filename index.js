c7701f362cbe723f5653438318636578dedd2116
Sachini <sachichathudissanayaka@gmail.com>
2024-10-12 20:33:14 +0530

    Add changes

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const images = document.querySelectorAll('#img');
-const imgEl = document.getElementById('img-el')
+const imgEl = document.getElementById('img-el') 
+const imagesEl = document.getElementById('images')
 
-images.forEach(image => {
-    image.addEventListener('click', function() {
-        const imgSrc = this.getAttribute('src');
-        imgEl.innerHTML = `<img class="px-1 py-1" src=${imgSrc} style="width: 100%;">`
+function renderImages(){
+    for(let i=1; i<=40; i++){
+        imagesEl.innerHTML += `<img id="img" class="col-sm-6 col-md-3 mb-3 mb-sm-4" src="img/img-${i}.jpeg" data-bs-toggle="modal"
+                    data-bs-target="#exampleModal">`
+    }
+
+    document.querySelectorAll('#img').forEach(image => {
+        image.addEventListener('click', function() {
+            const imgSrc = this.getAttribute('src');
+            imgEl.innerHTML = `<img class="px-1 py-1" src=${imgSrc} style="width: 100%;">`
+        });
     });
-});
+}
+
+renderImages()
+
 
